15cbb261248deb963605ee08d30ba429d7e3fa16
Ricky <113133454+murphyswebdesigns@users.noreply.github.com>
2024-02-28 22:16:19 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+import TopNav from "./TopNav";
+
+export default function App() {
+  return (
+    <div className="App">
+      <TopNav />
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
